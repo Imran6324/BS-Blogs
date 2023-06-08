07186f109b43959a1981d1ca9b39cc4c86fde574
Imran <khanmohammadimran63@gmail.com>
2023-06-08 17:41:31 +0530

    all commit

---
+++ src/config/data.js
@@ -0,0 +1,87 @@
+export const blogList = [
+  {
+    id: 1,
+    title: 'How coffee helps lower type 2 diabetes risk: New clues on mechanism ',
+    category: 'health',
+    subCategory: ['food', 'daily', 'routine'],
+    description:
+      "The study’s authors analyzed a large data set from participants in two population-based studies: the United Kingdom’s UK Biobank and the Netherlands’ Rotterdam Study.The UK Biobank cohort included 502,536 individuals from England, Scotland, and Wales who began participating in the study between April 2006 and December 2010. They were 37 to 73 years old. In 2017, follow-up data on these individuals became available.The Rotterdam Study, which began in 1990, is ongoing, ultimately involving 14,929 individuals. Follow-up data was released in 2015.In the study, researchers observed changes in the levels of inflammation-related biomarkers related to type 2 diabetes.Lead author Dr. Carolina Ochoa-Rosales, a postdoctoral scientist in nutrition and genetic epidemiology at Erasmus University Medical Centre Rotterdam, in the Netherlands, told Medical News Today that “[t]ype 2 diabetes is partly considered an inflammatory disease, and it is accepted that higher concentrations of pro-inflammatory markers in plasma constitute a risk factor for type 2 diabetes.”The study found that drinking coffee boosts levels of the anti-inflammatory adipokine hormone adiponectinTrusted Source, as well as cytokine interleukin-13, or IL-13. At the same time, coffee consumption decreases levels of C-reactive protein (CRP) and leptin, which promote inflammation.“CRP is an inflammation marker that reflects intracellular damage,” explained endocrinologist Dr. Ana Maria Kausel, who was not involved in the study.",
+    authorName: 'Robby Berman',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/cover4.png',
+  },
+  {
+    id: 2,
+    title: 'Is Diabetes Becoming the Biggest Epidemic of the Twenty-first Century?',
+    category: 'hots',
+    subCategory: ['diabeties', 'health', 'lifestyle'],
+    description:
+      "Diabetes is a major public health problem that is approaching epidemic proportions globally. Worldwide, the prevalence of chronic, noncommunicable diseases is increasing at an alarming rate. About 18 million people die every year from cardiovascular disease, for which diabetes and hypertension are major predisposing factors. Today, more than 1.7 billion adults worldwide are overweight, and 312 million of them are obese. In addition, at least 155 million children worldwide are overweight or obese. A diabetes epidemic is underway. According to an estimate of International Diabetes Federation comparative prevalence of Diabetes during 2007 is 8.0 % and likely to increase to 7.3% by 2025. Number of people with diabetes is 246 million (with 46% of all those affected in the 40–59 age group) and likely to increase to 380 m by 2025. The comparative prevalence of IGT is 7.5% in 2007 and likely to go up to 6.0 by 2025. The number of people with IGT is 308 million in 2007 and likely to be 418 m by 2025. (1) Almost 80% of the total adult diabetics are in developing countries. The regions with the highest rates are the Eastern Mediterranean and Middle East, where 9.2 % of the adult population is affected, and North America (8.4%). The highest numbers, however, are found in the Western Pacific, where some 67 million people have Diabetes, followed by Europe with 53 million. India leads the global top ten in terms of the highest number of people with diabetes with a current figure of 40.9 million, followed by China with 39.8 million. Behind them come USA; Russia; Germany; Japan; Pakistan; Brazil; Mexico and Egypt.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/cover1.jpeg',
+  },
+  {
+    id: 3,
+    title: 'Should you be eating 30 plants a week to stay healthy? The evidence says very little',
+    category: 'routine',
+    subCategory: ['e-commerce store', 'clothing', 'shopping store'],
+    description:
+      "Health influencers love to feed their followers snappy-sounding advice and if it has a number in it, even better.Take the frequently dished out guidance to drink eight glasses of water a day, for example. Nobody really knows where this piece of advice originated from but almost every adult has heard it – in one measurement or another.However, it is completely unnecessary to drink that much water every day. Doctors say that if you aren’t feeling thirsty all the time, you’re probably getting enough to drink, and one 2022 study concluded that everyone is different. “One size does not fit all for drinking water guidelines,” it stated, adding that the belief that everyone needs eight glasses a day “is not backed up by objective evidence”.",
+    authorName: 'Stuart Ritchie',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/cover2.jpg',
+  },
+  {
+    id: 4,
+    title: 'Unveiling the Mysteries of the Amazon: A Thrilling Adventure Into the Heart of the Rainforest',
+    category: 'adventure',
+    subCategory: ['adrenaline', 'stay-fit', 'lifestyle'],
+    description:
+      "The Amazon rainforest, with its lush green canopy, diverse wildlife, and enigmatic aura, beckons adventure enthusiasts to embark on an extraordinary journey into one of the world's most captivating wildernesses. Brace yourself for an immersive experience as you delve deep into the heart of the Amazon, unraveling its mysteries and encountering its hidden wonders. Your adventure begins with a riverboat expedition along the mighty Amazon River, navigating through winding tributaries and witnessing the vibrant tapestry of life that thrives along its banks. As you venture further into the dense jungle, accompanied by knowledgeable guides, prepare to encounter exotic creatures like colorful macaws, playful monkeys, and stealthy jaguars. Immerse yourself in the rich indigenous cultures that call the Amazon home, learning their ancient traditions and gaining a profound appreciation for the delicate balance between nature and humanity. Trek through the labyrinthine trails, where every step reveals a new marvel, from towering trees adorned with dangling lianas to vibrant orchids hidden amidst the foliage. Dive into the crystal-clear waters of pristine Amazonian rivers, swimming alongside friendly pink dolphins and exploring vibrant underwater ecosystems. Spend nights in eco-lodges nestled deep within the rainforest, falling asleep to the symphony of nocturnal creatures and awakening to the melodious calls of tropical birds. Let the vastness and raw beauty of the Amazon envelop your senses as you forge unforgettable memories and return home with a newfound respect for the power and fragility of nature.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/graphic-design-trends.png',
+  },
+  {
+    id: 5,
+    title: 'The Art of Culinary Magic: Mastering the Science and Creativity of Cooking',
+    category: 'cooking',
+    subCategory: ['bbq', 'food', 'lifestyle'],
+    description:
+      "In the realm of cooking, science acts as the guiding force that unveils the secrets behind culinary transformations. It is through understanding the properties of ingredients, chemical reactions, and cooking methods that we can create gastronomic wonders. The Maillard reaction, for instance, is the enchanting process that occurs when proteins and sugars react, giving birth to a symphony of flavors and that irresistible golden crust on a perfectly seared steak. Similarly, the emulsification process relies on the science of combining oil and water-based ingredients to create creamy sauces or dressings. Yet, cooking is not solely confined to the boundaries of scientific precision. It thrives on the boundless realm of creativity, encouraging culinary enthusiasts to experiment, innovate, and think outside the recipe book. Like an artist with a blank canvas, a cook's palette comprises ingredients from various cultures, each offering a unique flavor profile and story. By blending diverse culinary traditions, we can craft fusion dishes that tantalize the taste buds and reflect the ever-evolving nature of our global society.Moreover, cooking transcends the mere act of nourishment, transcending cultural and social barriers to become a language of its own. It brings people together, creating shared experiences and cherished memories. The act of preparing a meal for loved ones becomes an expression of love and care, while hosting a dinner party becomes a celebration of friendship and connection. The kitchen serves as a sanctuary, where aromas intertwine, laughter echoes, and conversations flow as effortlessly as the culinary creations themselves.As we embark on our own culinary adventures, let us embrace the spirit of both the scientist and the artist. By understanding the scientific principles that underpin cooking techniques, we gain the power to achieve consistent and exceptional results. Simultaneously, we must liberate our imaginations and unleash our creativity, fearlessly experimenting with flavors, textures, and presentations. In this delicate dance between science and art, we discover the true magic of cooking—an alchemy that transforms humble ingredients into extraordinary experiences.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/make-it-personal.jpg',
+  },
+  {
+    id: 6,
+    title: 'Beyond the Beach: Unveiling the Hidden Treasures of Travel',
+    category: 'travel',
+    subCategory: ['beaches', 'sea', 'holidays'],
+    description:
+      "When we think of travel, beach destinations often come to mind—a picturesque coastline, azure waters, and endless sun-kissed days. While the allure of sandy shores is undeniable, there is a whole world waiting to be explored beyond the beach. Delving into the heart of a destination unveils hidden treasures that offer a deeper understanding of its culture, history, and natural wonders. Venturing beyond the beach opens up a myriad of possibilities for authentic and immersive experiences. Step into the bustling streets of a vibrant city, where the rhythm of life pulses through its markets, architecture, and cuisine. Engage with locals, exchange stories, and immerse yourself in the customs and traditions that shape their lives. Wander through ancient cobblestone alleyways, discovering hidden gems, and stumbling upon architectural marvels that whisper tales of a bygone era. Nature lovers will find their paradise in the unexplored landscapes that lie just beyond the shoreline. Embark on breathtaking hikes through lush forests, where towering trees and vibrant flora create a symphony of colors and scents. Seek out hidden waterfalls, crystal-clear lakes, and remote mountain peaks that offer awe-inspiring vistas and moments of tranquility. Let the untouched wilderness inspire a sense of wonder and reconnect you with the natural world. Unearthing a destination's history is another rewarding aspect of venturing beyond the beach. Explore ancient ruins that bear witness to the rise and fall of civilizations, unraveling mysteries and marveling at architectural feats. Wander through museums and galleries that showcase the artistic and intellectual legacy of a place, allowing history to come alive through the brushstrokes of masterpieces and the artifacts of yesteryears. Beyond the beach lies a culinary world waiting to be savored.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover:
+      '/assets/images/Purple-Combination-colors-graphic-design-predictions-1024x576-1024x576.jpg',
+  },
+  {
+    id: 7,
+    title: 'The Power of Art: Igniting Creativity, Inspiring Change',
+    category: 'art',
+    subCategory: ['skill', 'design', 'passion'],
+    description:
+      "Art has always been a profound form of expression, transcending boundaries, cultures, and languages. It has the unique ability to evoke emotions, challenge perspectives, and ignite the spark of creativity within us. Whether it's a captivating painting, a mesmerizing sculpture, or a thought-provoking installation, art has the power to inspire change and shape the world we live in. Artistic expression allows individuals to communicate their thoughts, ideas, and emotions in ways that words often fail to capture. It provides a platform for artists to delve into the depths of their imagination, exploring the beauty, complexity, and diversity of the human experience. Through their art, they give voice to the silenced, challenge societal norms, and create a visual narrative that transcends time. Moreover, art serves as a catalyst for change, provoking conversations and pushing boundaries. It has the ability to shed light on social, political, and environmental issues that demand attention. Artists have used their creative prowess to challenge injustices, advocate for equality, and amplify marginalized voices. By harnessing the power of art, they awaken society's consciousness, urging us to reflect, question, and take action. Art also nourishes our souls, offering solace and inspiration in times of adversity. It has the capacity to heal, providing an outlet for emotional expression and offering a sanctuary for introspection. Whether we're marveling at a masterpiece in a gallery, attending a live performance, or creating art ourselves, we tap into a profound sense of connection and find solace in the beauty and intricacy of the artistic world.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/Synthwave-Postmodern.jpg',
+  },
+];
